6ea3266a7c625d40598d6e7ead28236338f56b68
Lucho Ramos <lramos@MacBook-Pro-de-Lucho-imbee.local>
2023-06-01 12:42:51 +0200

    feat: improve logs

--- src/go.mod
+++ src/go.mod
@@ -1,6 +1,6 @@
 module github.com/lucian0ramos/image-golang/src
 
-go 1.16
+go 1.18
 
 require (
 	github.com/google/uuid v1.3.0
@@ -8,4 +8,23 @@ require (
 	github.com/rs/zerolog v1.29.1
 )
 
-require github.com/go-sql-driver/mysql v1.7.1
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/justinas/alice v1.2.0
+	github.com/prometheus/client_golang v1.15.1
+)
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.42.0 // indirect
+	github.com/prometheus/procfs v0.9.0 // indirect
+	github.com/rs/xid v1.4.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
--- src/main.go
+++ src/main.go
@@ -9,17 +9,64 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/justinas/alice"
 	"github.com/lucian0ramos/image-golang/src/handlers"
 	"github.com/lucian0ramos/image-golang/src/middlewares"
 	"github.com/lucian0ramos/image-golang/src/models"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/hlog"
 )
 
 var (
 	addr = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
+
+	inFlightGauge = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "in_flight_requests",
+			Help: "A gauge of requests currently being served by the wrapped handler.",
+		},
+	)
+
+	counter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "A counter for requests to the wrapped handler.",
+			ConstLabels: map[string]string{
+				"version": version,
+			},
+		},
+		[]string{"code", "method"},
+	)
+
+	duration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "request_duration_seconds",
+			Help:    "A histogram of latencies for requests.",
+			Buckets: []float64{.25, .5, 1, 2.5, 5, 10},
+			ConstLabels: map[string]string{
+				"version": version,
+			},
+		},
+		[]string{"code", "method"},
+	)
+
+	responseSize = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "response_size_bytes",
+			Help:    "A histogram of response sizes for requests.",
+			Buckets: []float64{200, 500, 900, 1500},
+			ConstLabels: map[string]string{
+				"version": version,
+			},
+		},
+		[]string{"code", "method"},
+	)
+	version string
 )
 
 func main() {
@@ -42,10 +89,15 @@ func main() {
 
 	//router
 	r := mux.NewRouter()
+	c := alice.New(hlog.NewHandler(log), hlog.AccessHandler(accessLogger))
+	c = c.Append(hlog.RemoteAddrHandler("ip"))
+	c = c.Append(hlog.UserAgentHandler("user_agent"))
+	c = c.Append(hlog.RequestIDHandler("req_id", "Request-Id"))
+
 	r.HandleFunc("/manage-errors", middlewares.Chain(handlers.ManageErrors, middlewares.ValidateContentType(), middlewares.ValidateAuthorization())).Methods("POST")
 
 	srv := &http.Server{
-		Handler: r,
+		Handler: c.Then(promRequestHandler(r)),
 		Addr:    *addr,
 	}
 	go serveHTTP(ctx, log, srv)
@@ -68,4 +120,25 @@ func serveHTTP(ctx context.Context, log zerolog.Logger, srv *http.Server) {
 	if err != http.ErrServerClosed {
 		log.Error().Caller().Interface("event_id", ctx.Value(models.IDKey{})).Err(err).Msg("starting Server listener failed")
 	}
+}
+
+func accessLogger(r *http.Request, status, size int, dur time.Duration) {
+	hlog.FromRequest(r).Info().
+		Str("host", r.Host).
+		Int("status", status).
+		Str("url", r.RequestURI).
+		Str("method", r.Method).
+		Int("size", size).
+		Dur("duration_ms", dur).
+		Msg("request")
+}
+
+func promRequestHandler(handler http.Handler) http.Handler {
+	return promhttp.InstrumentHandlerInFlight(inFlightGauge,
+		promhttp.InstrumentHandlerDuration(duration,
+			promhttp.InstrumentHandlerCounter(counter,
+				promhttp.InstrumentHandlerResponseSize(responseSize, handler),
+			),
+		),
+	)
 }
